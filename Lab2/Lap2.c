0fc8487ecd846d5df369a0fdf061f999dd242bbd
PS49716-NguyenVanDuy <nguyenvanduy207nd@gmail.com>
2025-11-11 13:13:29 +0700

    nộp bài lap2

--- Lab2/Lap2.c
+++ Lab2/Lap2.c
@@ -19,7 +19,7 @@ int main(){
     return 0;
 }*/
 //Bài 3: XÂY DỰNG CHƯƠNG TRÌNH TÍNH CHU VI DIỆN TÍCH HÌNH TRÒN
-#include <stdio.h>
+/*#include <stdio.h>
 #define PI 3.14
 int main(){
     float r;
@@ -27,4 +27,31 @@ int main(){
     scanf("%f",&r);
     printf("Chu vi hinh tron la: %.2f\n",2*PI*r);
     printf("Dien tich hinh tron la: %.2f\n",PI*r*r);
+}*/
+//Bài 4: XÂY DỰNG CHƯƠNG TRÌNH TÍNH ĐIỂM TRUNG BÌNH 
+#include <stdio.h>
+int main (){
+    float toan, ly, hoa;
+    printf("Nhap diem toan: ");
+    scanf("%f", &toan);
+    printf("Nhap diem ly: ");
+    scanf("%f",&ly);
+    printf("Nhap diem hoa: ");
+    scanf("%f",&hoa);
+    float tong = toan*3 + ly*2 + hoa;
+    float diemTB = tong / 6; // Chia cho 6.0
+    printf("Diem trung binh la: %.2f\n", diemTB);
+//Bai 5: tinhs chu vi dien tich tam giac vuong
+    float chieudai, chieurong, canhhuyen;
+    printf("Nhap chieu dai: ");
+    scanf("%f", &chieudai);
+    printf("Nhap chieu rong: ");
+    scanf("%f", &chieurong);
+    printf("Nhap canh huyen: ");
+    scanf("%f", &canhhuyen);
+    float chuvi=(chieudai + chieurong + canhhuyen);
+    float dientich = (chieudai * chieurong)/2;
+    printf("Chu vi hinh chu nhat la: %.2f cm\n", chuvi);
+    printf("Dien tich hinh chu nhat la: %.2f cm\n", dientich);
+    return 0;
 }
